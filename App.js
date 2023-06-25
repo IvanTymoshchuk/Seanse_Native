65131f82eb198a8df3f88a68a233acac01462877
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-06-25 16:24:14 +0200

    first filles

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Registration } from "./Screens/RegistrationScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Registration />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Screens/RegistrationScreen.js
@@ -0,0 +1,188 @@
+import { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  ImageBackground,
+  Alert,
+} from "react-native";
+
+export const Registration = () => {
+  const [login, onChangeLogin] = useState("");
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+  const [showPassword, setShowPassword] = useState(true);
+  const [isFocusedEmail, setIsFocusedEmail] = useState(false);
+  const [isFocusedPassword, setIsFocusedPassword] = useState(false);
+  const [isFocusedLogin, setIsFocusedLogin] = useState(false);
+
+  const behavior = Platform.OS === "ios" ? "padding" : "height";
+  const keyboardVerticalOffset = Platform.OS === "ios" ? -140 : -136;
+
+  const togleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <ImageBackground
+      style={styles.imageBg}
+      source={require("../assets/img/BG.jpg")}
+    >
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={{ flex: 1, justifyContent: "flex-end" }}>
+          <KeyboardAvoidingView
+            behavior={behavior}
+            keyboardVerticalOffset={keyboardVerticalOffset}
+          >
+            <View style={styles.form}>
+              <Text style={styles.formTitle}>Реєстрація</Text>
+              <View>
+                <TextInput
+                  value={login}
+                  onChangeText={onChangeLogin}
+                  style={[
+                    styles.input,
+                    {
+                      borderColor: isFocusedLogin ? "#FFA500" : "#ccc",
+                      borderWidth: isFocusedLogin ? 2 : 1,
+                    },
+                  ]}
+                  onFocus={() => setIsFocusedLogin(true)}
+                  onBlur={() => setIsFocusedLogin(false)}
+                  placeholder="Логін"
+                />
+              </View>
+
+              <View>
+                <TextInput
+                  value={email}
+                  onChangeText={onChangeEmail}
+                  style={[
+                    styles.input,
+                    {
+                      borderColor: isFocusedEmail ? "#FFA500" : "#ccc",
+                      borderWidth: isFocusedEmail ? 2 : 1,
+                    },
+                  ]}
+                  onFocus={() => setIsFocusedEmail(true)}
+                  onBlur={() => setIsFocusedEmail(false)}
+                  placeholder="Адреса електронної пошти"
+                />
+              </View>
+              <View style={{ position: "relative" }}>
+                <TextInput
+                  value={password}
+                  onChangeText={onChangePassword}
+                  secureTextEntry={showPassword}
+                  style={[
+                    styles.input,
+                    {
+                      borderColor: isFocusedPassword ? "#FFA500" : "#ccc",
+                      borderWidth: isFocusedPassword ? 2 : 1,
+                    },
+                  ]}
+                  onFocus={() => setIsFocusedPassword(true)}
+                  onBlur={() => setIsFocusedPassword(false)}
+                  placeholder="Пароль"
+                />
+                <TouchableOpacity
+                  onPress={togleShowPassword}
+                  activeOpacity={0.7}
+                  style={styles.showPasswordWrap}
+                >
+                  <Text style={styles.showPasswordTitle}>
+                    {showPassword ? "Показати" : "Приховати"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity
+                onPress={() => Alert.alert("Work")}
+                activeOpacity={0.7}
+                style={styles.btn}
+              >
+                <Text style={styles.btnTitle}>Зареєструватися</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                onPress={() => Alert.alert("Work ask")}
+              >
+                <Text style={styles.linkTitle}>Вже є аккаунт? Увійти</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </View>
+      </TouchableWithoutFeedback>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageBg: { flex: 1, width: "100%" },
+  form: {
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: 16,
+    paddingTop: 90,
+    paddingBottom: 45,
+    position: "relative",
+  },
+
+  formTitle: {
+    fontSize: 30,
+    fontWeight: 500,
+    lineHeight: 35,
+    textAlign: "center",
+    color: "#212121",
+    marginBottom: 20,
+  },
+
+  input: {
+    height: 50,
+    marginTop: 12,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: "#F6F6F6",
+  },
+
+  btn: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+  },
+
+  btnTitle: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#FFFFFF",
+  },
+  linkTitle: {
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#1B4371",
+    marginTop: 16,
+  },
+  showPasswordWrap: {
+    position: "absolute",
+    top: 28,
+    right: 16,
+  },
+
+  showPasswordTitle: {
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
