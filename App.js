28e5ef88ca3d777624d52347987e5e6cdf8ca1ff
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-07-09 13:27:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
